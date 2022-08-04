6a857300712143af5d74dcfbdff5db9b9bb7446f
Anshul <akb.tech17@gmail.com>
2022-08-04 15:20:40 +0530

    interpolation

--- demoangular/src/app/mycomponent/mycomponent.component.ts
+++ demoangular/src/app/mycomponent/mycomponent.component.ts
@@ -8,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class MycomponentComponent implements OnInit {
   name: string = 'Tom'
   age: number = 30
+  fno: number = 70
+  sno: number = 30
+  total: number = 0
+
+  add(): number {
+    this.total = this.fno + this.sno
+    return this.total
+  }
 
   constructor() { }
 
