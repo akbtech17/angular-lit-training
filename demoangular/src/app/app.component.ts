310d9ade936131ccf662a6d97d83cb9c90aa24ff
Anshul <akb.tech17@gmail.com>
2022-08-04 12:44:19 +0530

    starting fresh

--- demoangular/src/app/app.component.ts
+++ demoangular/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+// Component: Decorator
+
+// Selector: Defines the identifie for the component
+// templateUrl: for html
+// styleUrls: styling file
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// exporting the class 
 export class AppComponent {
   title = 'demoangular';
 }
