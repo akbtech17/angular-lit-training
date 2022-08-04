eb77eb1481294ed7a5175c2323f28799076e0a5e
Anshul <akb.tech17@gmail.com>
2022-08-04 14:18:51 +0530

    data binding ts with html

--- demoangular/src/app/mycomponent/mycomponent.component.ts
+++ demoangular/src/app/mycomponent/mycomponent.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mycomponent.component.css']
 })
 export class MycomponentComponent implements OnInit {
-  
+  name: string = 'Tom'
+  age: number = 30
+
   constructor() { }
 
   ngOnInit(): void {
+  
   }
-
 }
