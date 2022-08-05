d5d70af29042b0d8e81a0c0ddc175b96d9e1d202
Anshul <akb.tech17@gmail.com>
2022-08-05 09:45:53 +0530

    added routes in routes array of routing ts file

--- demoangular/src/app/app-routing.module.ts
+++ demoangular/src/app/app-routing.module.ts
@@ -3,8 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MycomponentComponent } from './mycomponent/mycomponent.component';
 import { EmplistComponent } from './emplist/emplist.component';
+
 // decalre all the links and components here which are required in routing
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'mycomponent', component: MycomponentComponent},
+  {path: 'emplist', component: EmplistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
