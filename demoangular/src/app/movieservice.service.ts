a3aa7f4db08d33e2d4d3b8bf3d33237bb944e473
Anshul <akb.tech17@gmail.com>
2022-08-05 14:16:42 +0530

    created a method in export class of a movieservice

--- demoangular/src/app/movieservice.service.ts
+++ demoangular/src/app/movieservice.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 
+// injectable - means this class can only be injected
 @Injectable({
   providedIn: 'root'
 })
 export class MovieserviceService {
-
+  Display(name: string): string {
+    return 'Hello '+name
+  }
   constructor() { }
 }
