509858fc49bdc87468a3d24e4bf87ea1723fce93
Anshul <akb.tech17@gmail.com>
2022-08-05 09:42:57 +0530

    import all components in app-routing module ts file

--- demoangular/src/app/app-routing.module.ts
+++ demoangular/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
+import { MycomponentComponent } from './mycomponent/mycomponent.component';
+import { EmplistComponent } from './emplist/emplist.component';
 // decalre all the links and components here which are required in routing
 const routes: Routes = [];
 
