d3c6a8e00daf8ac333590f223eb578efbd207d3f
Anshul <akb.tech17@gmail.com>
2022-08-05 10:36:42 +0530

    defined routing of movie component in app-routing module

--- demoangular/src/app/app-routing.module.ts
+++ demoangular/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MycomponentComponent } from './mycomponent/mycomponent.component';
 import { EmplistComponent } from './emplist/emplist.component';
+import { MovieaddComponent } from './movieadd/movieadd.component';
 
 // decalre all the links and components here which are required in routing
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'mycomponent', component: MycomponentComponent},
-  {path: 'emplist', component: EmplistComponent}
+  {path: 'emplist', component: EmplistComponent},
+  {path: 'movieadd', component: MovieaddComponent}
 ];
 
 @NgModule({
