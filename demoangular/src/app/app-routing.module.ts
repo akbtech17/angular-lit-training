07cdaf936e0f3e110677043554549611ec8d6a63
Anshul <akb.tech17@gmail.com>
2022-08-05 09:41:54 +0530

    starting with routing

--- demoangular/src/app/app-routing.module.ts
+++ demoangular/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// decalre all the links and components here which are required in routing
 const routes: Routes = [];
 
 @NgModule({
