e69be98510e5576196cc0a1135f40dd15a793edf
Anshul <akb.tech17@gmail.com>
2022-08-05 10:48:03 +0530

    created method movieAdd

--- demoangular/src/app/movieadd/movieadd.component.ts
+++ demoangular/src/app/movieadd/movieadd.component.ts
@@ -7,12 +7,20 @@ import { Movie } from '../movie';
   styleUrls: ['./movieadd.component.css']
 })
 export class MovieaddComponent implements OnInit {
-  movieData: Movie[] = [
-    {id: 0, name:'Sultan', year:2000, rating:3},
-    {id: 1, name:'DDLJ', year:1999, rating:4},
-    {id: 2, name:'Mai Hoon Na', year:2009, rating:5},
-    {id: 3, name:'LOC', year:2022, rating:2}
-  ];
+  // movieData: Movie[] = [
+  //   {id: 0, name:'Sultan', year:2000, rating:3},
+  //   {id: 1, name:'DDLJ', year:1999, rating:4},
+  //   {id: 2, name:'Mai Hoon Na', year:2009, rating:5},
+  //   {id: 3, name:'LOC', year:2022, rating:2}
+  // ];
+
+  movieData: Movie = {id: 0, name:'', year:0, rating:0}
+
+  SaveData(data: Movie) {
+    this.movieData = data;
+    console.log(this.movieData);
+  } 
+
   constructor() { }
 
   ngOnInit(): void {
